dff7251f3999515271a496fe22ed05688abe8134
jonflix mgr <support@trilm.com>
2019-03-23 12:06:26 +0000

    Dockerfile updates

--- ebrd-proxy.sh
+++
@@ -1,5 +0,0 @@
-# source /ebrd-proxy.sh
-
-export http_proxy="http://ldn3log1.ebrd.com:8888"
-export https_proxy="http://ldn3log1.ebrd.com:8888"
-export no_proxy="ldn1cvs2.ebrd.com,localhost,docker,docker:2375"
